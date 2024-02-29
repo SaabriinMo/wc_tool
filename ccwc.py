6bbc43c32a0ac29b900f4dda44ed757fc56dbd91
SaabriinMo <saabriinm123@gmail.com>
2024-02-29 20:02:42 +0000

    rearranging outputs

--- ccwc.py
+++ ccwc.py
@@ -18,19 +18,19 @@ def argparse_ccwc():
 def output(args):
     result = []
     if args.c:
-        result.append(wc_tool.wc_byte_count(args.file))
+        result.extend([wc_tool.wc_byte_count(args.file),args.file])
     if args.l:
-        result.append(wc_tool.wc_line_count(args.file, ))
+        result.extend([wc_tool.wc_line_count(args.file),args.file])
     if args.w:
-        result.append(wc_tool.wc_word_count(args.file))
+        result.extend([wc_tool.wc_word_count(args.file),args.file])
     if args.m:
-        result.append(wc_tool.wc_char_count(args.file))
+        result.extend([wc_tool.wc_char_count(args.file),args.file])
 
     if not any([args.c, args.l, args.w, args.m]):
         result.extend([args.file, wc_tool.wc_byte_count(args.file), wc_tool.wc_line_count(args.file, ),
                        wc_tool.wc_word_count(args.file)])
 
-    return " ".join(map(str, result))
+    return "  " + " ".join(map(str, result))
 
 
 def main():
